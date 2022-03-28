5cfc186027a5c4ecb3a52595389694eaea0f8b2e
Ondřej Hájek <ondra.hajku@seznam.cz>
2022-03-28 18:21:56 +0200

    Patch[3.1]

--- src/CreateAccount.js
+++ src/CreateAccount.js
@@ -3,17 +3,17 @@ import Logo from "./Logo";
 import { useState } from "react";
 export default function CreateAccount() {
 
-  const [createProfile, setCreateProfile] = useState({name: "", age: null, email: "", password: "", password2: "", linence: false})
+  const [createProfile, setCreateProfile] = useState({name: "", age: "", email: "", password: "", password2: "", licence: false})
 
   function change(event){
     setCreateProfile(oldVal=>{
       return {
         ...oldVal,
-        [event.target.name]: event.target.name=="licence" ? event.target.checked : event.target.value
+        [event.target.name]: event.target.name==="licence" ? event.target.checked : event.target.value
       }
     })
   }
-
+  console.log(createProfile)
   return (
     <>
       <Nav />
@@ -24,32 +24,32 @@ export default function CreateAccount() {
         <form className="flex flex-col text-xl max-w-96 mx-auto">
           <div className="flex flex-wrap justify-between">
             <label className="my-auto mx-2 w-full sm:w-auto">Username</label>
-            <input className="m-2 p-1 w-full sm:w-auto border-2" name="name" type="text" onChange={change}/>
+            <input className="m-2 p-1 w-full sm:w-auto border-2" name="name" type="text" onChange={change} value={createProfile.name}/>
           </div>
 
           <div className="flex flex-wrap justify-between">
             <label className="my-auto mx-2 w-full sm:w-auto">Age</label>
-            <input className="m-2 p-1 w-full sm:w-auto border-2" name="age" type="number" onChange={change}/>
+            <input className="m-2 p-1 w-full sm:w-auto border-2" name="age" type="number" onChange={change} value={createProfile.age}/>
           </div>
 
           <div className="flex flex-wrap justify-between">
             <label className="my-auto mx-2 w-full sm:w-auto">Email</label>
-            <input className="m-2 p-1 w-full sm:w-auto border-2" name="email" type="text" onChange={change}/>
+            <input className="m-2 p-1 w-full sm:w-auto border-2" name="email" type="text" onChange={change} value={createProfile.email}/>
           </div>
 
           <div className="flex flex-wrap justify-between">
             <label className="my-auto mx-2 w-full sm:w-auto">Password</label>
-            <input className="m-2 p-1 w-full sm:w-auto border-2" name="password" type="password" onChange={change}/>
+            <input className="m-2 p-1 w-full sm:w-auto border-2" name="password" type="password" onChange={change} value={createProfile.password}/>
           </div>
 
           <div className="flex flex-wrap justify-between">
             <label className="my-auto mx-2 w-full sm:w-auto">Confirm password</label>
-            <input className="m-2 p-1 w-full sm:w-auto border-2" name="password2" type="password" onChange={change}/>
+            <input className="m-2 p-1 w-full sm:w-auto border-2" name="password2" type="password" onChange={change} value={createProfile.password2}/>
           </div>
 
           <div className="flex flex-wrap justify-between">
             <label className="my-auto mx-2 text-base">I agree with licence agreements</label>
-            <input className="m-2 p-1" type="checkbox" name="blig" onChange={change}/>
+            <input className="m-2 p-1" type="checkbox" name="licence" onChange={change} checked={createProfile.licence}/>
           </div>
           <button className=" p-2 bg-logo-blue text-white rounded-2xl mx-auto my-2 active:bg-dark-logo-blue">
             Register
--- src/LostPassword.js
+++ src/LostPassword.js
@@ -1,23 +1,35 @@
 import Nav from "./Nav";
 import Logo from "./Logo";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 export default function LostPassword() {
+
+  const [changePassword, setChangePassword]  = useState({name: "", email: ""})
+
+  function change(event){
+    setChangePassword(oldVal=>{
+      return {
+        ...oldVal,
+        [event.target.name]: event.target.value
+      }
+    })
+  }
+  console.log(changePassword)
   return (
     <>
       <Nav />
-
-<div className="flex flex-col w-11/12 mx-auto max-w-2xl my-10">
-  <Logo class="mx-auto my-4" />
+  <div className="flex flex-col w-11/12 mx-auto max-w-2xl my-10">
+    <Logo class="mx-auto my-4" />
 
   <form className="flex flex-col text-xl max-w-96 mx-auto">
     <div className="flex flex-wrap justify-between">
       <label className="my-auto mx-2 w-full sm:w-auto">Username</label>
-      <input className="m-2 p-1 w-full sm:w-auto border-2" type="text" />
+      <input className="m-2 p-1 w-full sm:w-auto border-2"  name="name" type="text" value={changePassword.name} onChange={change}/>
     </div>
 
     <div className="flex flex-wrap justify-between">
       <label className="my-auto mx-2 w-full sm:w-auto">Email</label>
-      <input className="m-2 p-1 w-full sm:w-auto border-2" type="email" />
+      <input className="m-2 p-1 w-full sm:w-auto border-2" name="email" type="email" value={changePassword.email} onChange={change}/>
     </div>
 
   
