db6b369ba75ab7ba4ec59092243ea5a3f54f12b2
Ondřej Hájek <ondra.hajku@seznam.cz>
2022-03-30 21:54:19 +0200

    Patch[6]

---
+++ src/Contribution.js
@@ -0,0 +1,32 @@
+import { nanoid } from "nanoid";
+export default function Contribution(props) {
+
+
+
+  const content = props.allContribution.map((item) => {
+    return (
+      <div className="flex border-4 mb-2" key={nanoid()}>
+
+        <div className="flex flex-col ">
+          <img src={require("./img.png")} alt="" className="my-2 mx-auto h-16 w-16 " />
+          <p className="text-base font-semibold text-center">14:45 15.5.2021</p>
+        </div>
+
+        <div className="flex w-full flex-col">
+          <div className="flex justify-between h-16 flex-wrap">
+          <h1 className="text-2xl my-auto mx-3">Ondřej Hájekffffffffff</h1>
+        </div>
+
+          <div>
+            <h1 className="text-xl font-semibold">{item.title}</h1>
+            <p>{item.content}</p>
+          </div>
+
+        </div>
+      </div>
+    );
+  });
+
+  return <>{content}</>;
+}
+
--- src/ScrollPage.js
+++ src/ScrollPage.js
@@ -1,163 +1,90 @@
+import { useState } from "react";
+import Contribution from "./Contribution";
+
 export default function ScrollPage() {
+
+  const [contribution, setContribution] = useState({title: "", content: ""})
+  const [allContribution, setAllContribution] = useState([])
+
+
+  function change(event){
+    setContribution(oldVal=>{
+      return {
+        ...oldVal,
+        [event.target.name]: event.target.value
+      }
+    })
+  }
+
+  function submit(event){
+    event.preventDefault()
+    setAllContribution(oldVal=>{
+      return [...oldVal, contribution]
+    })
+  }
+
   return (
     <div className=" w-full max-w-scroll-page mx-auto my-5 flex justify-between flex-wrap">
+      
+ 
       <div className="wrap:w-side-box p-2 border-4 order-1 w-full">
+
+
+
         <h1 className="font-semibold">You may know...</h1>
+          
+        <div className="flex wrap:flex-col flex-row wrap:overflow-y-auto overflow-y-scroll no-scrollbar">
 
-        <div className="flex wrap:flex-col flex-row flex-wrap">
-          <div className="flex my-2 wrap:w-auto w-64">
-            <img src={require("./img.png")} width="50" />
-            <div className="flex w-full flex-col ml-2 justify-between">
+          <div className="flex m-2 wrap:flex-row flex-col">
+            <img src={require("./img.png")} alt="" width="60" className="m-auto" />
+            <div className="w-full flex flex-col">
               <h1 className="text-center">Ondřej Hájek</h1>
-              <div className=" mx-auto px-2 rounded-md bg-logo-blue">
-                Follow
-              </div>
+              <div className="px-2 rounded-md bg-logo-blue m-auto ">Follow</div>
             </div>
-          </div>
 
-          <div className="flex my-2 wrap:w-auto w-64">
-            <img src={require("./img.png")} width="50" />
-            <div className="flex w-full flex-col ml-2 justify-between">
-              <h1 className="text-center">Ondřej Hájek</h1>
-              <div className=" mx-auto px-2 rounded-md bg-logo-blue">
-                Follow
-              </div>
-            </div>
-          </div>
 
-          <div className="flex my-2 wrap:w-auto w-64">
-            <img src={require("./img.png")} width="50" />
-            <div className="flex w-full flex-col ml-2 justify-between">
-              <h1 className="text-center">Ondřej Hájek</h1>
-              <div className=" mx-auto px-2 rounded-md bg-logo-blue">
-                Follow
-              </div>
-            </div>
-          </div>
 
-          <div className="flex my-2 wrap:w-auto w-64">
-            <img src={require("./img.png")} width="50" />
-            <div className="flex w-full flex-col ml-2 justify-between">
-              <h1 className="text-center">Ondřej Hájek</h1>
-              <div className=" mx-auto px-2 rounded-md bg-logo-blue">
-                Follow
-              </div>
-            </div>
-          </div>
-
-          <div className="flex my-2 wrap:w-auto w-64">
-            <img src={require("./img.png")} width="50" />
-            <div className="flex w-full flex-col ml-2 justify-between">
-              <h1 className="text-center">Ondřej Hájek</h1>
-              <div className=" mx-auto px-2 rounded-md bg-logo-blue">
-                Follow
-              </div>
-            </div>
-          </div>
         </div>
+
+      
+      </div>
       </div>
 
-      <div className="wrap:w-main-box w-full flex flex-col p-2 border-4 order-3 wrap:order-2">
-        <div className="flex flex-col w-full">
-          <label className="text-lg">Share how do you feel!</label>
-          <textarea className="w-full resize-none h-28 outline-none bg-logo-blue text-white p-1"></textarea>
+
+      <div className="wrap:w-main-box w-full flex flex-col order-3 wrap:order-2 m-2 wrap:m-0">
+        <form className="flex flex-col w-full" onSubmit={submit}>
+          <label className="text-xl font-semibold">Share how do you feel!</label>
+          <input type="text" className="bg-logo-blue text-white text-xl font-semibold outline-none p-1 mb-2" placeholder="Title..." onChange={change} value={contribution.title} name="title"/>
+          <textarea className="w-full resize-none h-28 outline-none bg-logo-blue text-white p-1" placeholder="Content..." onChange={change} value={contribution.content} name="content"></textarea>
           <div className="my-2">
             <button className="bg-logo-blue p-1 rounded-lg text-xl ml-auto">
               Post!
             </button>
           </div>
-        </div>
+        </form>
 
-        <div className="flex p-1 flex-col my-1">
-          <div className="flex flex-row justify-between w-full">
-            <div className="flex">
-              <img src={require("./img.png")} width="50" className="m-auto"/>
-              <h1 className="text-2xl my-auto mx-3">Ondřej Hájek</h1>
-            </div>
-
-            <p className="my-auto mx-3">10.3. 2012</p>
-          </div>
-          <div>
-            <h1 className="text-xl font-semibold">asfadsas</h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
-              omnis voluptatem consectetur consequuntur optio qui eligendi culpa
-              ipsum temporibus eos tempora error, fuga corrupti ex nulla
-              praesentium incidunt beatae et!
-            </p>
-          </div>
-        </div>
-
-        <div className="flex p-1 flex-col my-1">
-          <div className="flex flex-row justify-between w-full">
-            <div className="flex">
-              <img src={require("./img.png")} width="50" className="m-auto"/>
-              <h1 className="text-2xl my-auto mx-3">Ondřej Hájek</h1>
-            </div>
-
-            <p className="my-auto mx-3">10.3. 2012</p>
-          </div>
-          <div>
-            <h1 className="text-xl font-semibold">asfadsas</h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
-              omnis voluptatem consectetur consequuntur optio qui eligendi culpa
-              ipsum temporibus eos tempora error, fuga corrupti ex nulla
-              praesentium incidunt beatae et!
-            </p>
-          </div>
-        </div>
-
-        <div className="flex p-1 flex-col my-1">
-          <div className="flex flex-row justify-between w-full">
-            <div className="flex">
-              <img src={require("./img.png")} width="50" className="m-auto"/>
-              <h1 className="text-2xl my-auto mx-3">Ondřej Hájek</h1>
-            </div>
-
-            <p className="my-auto mx-3">10.3. 2012</p>
-          </div>
-          <div>
-            <h1 className="text-xl font-semibold">asfadsas</h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
-              omnis voluptatem consectetur consequuntur optio qui eligendi culpa
-              ipsum temporibus eos tempora error, fuga corrupti ex nulla
-              praesentium incidunt beatae et!
-            </p>
-          </div>
+        <div className="flex p-1 flex-col my-1 ">
+          <Contribution allContribution={allContribution} />
         </div>
       </div>
 
-      <div className="wrap:w-side-box border-4 p-2 order-2 wrap:order-3 w-full">
+
+<div className="wrap:w-side-box border-4 p-2 order-2 wrap:order-3 w-full h-min">
         <h1 className="font-semibold">Groups...</h1>
 
-        <div className="flex wrap:flex-col flex-row flex-wrap">
-          <div className="flex my-2 wrap:w-auto w-64">
-            <img src={require("./img.png")} width="50" />
-            <div className="flex w-full flex-col ml-2 justify-between">
-              <h1 className="text-center">Murdria</h1>
-              <div className=" mx-auto px-2 rounded-md bg-logo-blue">Join</div>
-            </div>
-          </div>
+        <div className="flex wrap:flex-col flex-row wrap:overflow-y-auto overflow-y-scroll no-scrollbar">
 
-          <div className="flex my-2 wrap:w-auto w-64">
-            <img src={require("./img.png")} width="50" />
-            <div className="flex w-full flex-col ml-2 justify-between">
-              <h1 className="text-center">Murdria</h1>
-              <div className=" mx-auto px-2 rounded-md bg-logo-blue">Join</div>
-            </div>
-          </div>
 
-          <div className="flex my-2 wrap:w-auto w-64">
-            <img src={require("./img.png")} width="50" />
-            <div className="flex w-full flex-col ml-2 justify-between">
+          <div className="flex m-2 wrap:flex-row flex-col">
+            <img src={require("./img.png")} alt="" width="60" className="m-auto" />
+            <div className="w-full flex flex-col">
               <h1 className="text-center">Murdria</h1>
-              <div className=" mx-auto px-2 rounded-md bg-logo-blue">Join</div>
+              <div className="px-2 rounded-md bg-logo-blue m-auto">Follow</div>
             </div>
           </div>
-        </div>
       </div>
+</div>
+
     </div>
   );
 }
