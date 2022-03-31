c64958ee95b5bf8ffaba73d49eae2e792e53d2cd
Ondřej Hájek <ondra.hajku@seznam.cz>
2022-03-31 07:32:00 +0200

    Patch[6.1]

--- src/Contribution.js
+++ src/Contribution.js
@@ -1,12 +1,8 @@
 import { nanoid } from "nanoid";
 export default function Contribution(props) {
-
-
-
   const content = props.allContribution.map((item) => {
     return (
       <div className="flex border-4 mb-2" key={nanoid()}>
-
         <div className="flex flex-col ">
           <img src={require("./img.png")} alt="" className="my-2 mx-auto h-16 w-16 " />
           <p className="text-base font-semibold text-center">14:45 15.5.2021</p>
@@ -14,14 +10,13 @@ export default function Contribution(props) {
 
         <div className="flex w-full flex-col">
           <div className="flex justify-between h-16 flex-wrap">
-          <h1 className="text-2xl my-auto mx-3">Ondřej Hájekffffffffff</h1>
-        </div>
+            <h1 className="text-2xl my-auto mx-3">Ondřej Hájek</h1>
+          </div>
 
           <div>
             <h1 className="text-xl font-semibold">{item.title}</h1>
             <p>{item.content}</p>
           </div>
-
         </div>
       </div>
     );
@@ -29,4 +24,3 @@ export default function Contribution(props) {
 
   return <>{content}</>;
 }
-
--- src/ScrollPage.js
+++ src/ScrollPage.js
@@ -2,38 +2,38 @@ import { useState } from "react";
 import Contribution from "./Contribution";
 
 export default function ScrollPage() {
+  const [contribution, setContribution] = useState({ title: "", content: "" });
+  const [allContribution, setAllContribution] = useState([]);
 
-  const [contribution, setContribution] = useState({title: "", content: ""})
-  const [allContribution, setAllContribution] = useState([])
-
-
-  function change(event){
-    setContribution(oldVal=>{
+  function change(event) {
+    setContribution((oldVal) => {
       return {
         ...oldVal,
-        [event.target.name]: event.target.value
-      }
-    })
+        [event.target.name]: event.target.value,
+      };
+    });
   }
 
-  function submit(event){
-    event.preventDefault()
-    setAllContribution(oldVal=>{
-      return [...oldVal, contribution]
-    })
+  function submit(event) {
+    event.preventDefault();
+    setAllContribution((oldVal) => {
+      return [...oldVal, contribution];
+    });
   }
 
   return (
     <div className=" w-full max-w-scroll-page mx-auto my-5 flex justify-between flex-wrap">
-      
- 
-      <div className="wrap:w-side-box p-2 border-4 order-1 w-full">
-
-
-
+      <div className="wrap:w-side-box p-2 border-4 order-1 w-1/2">
         <h1 className="font-semibold">You may know...</h1>
-          
+
         <div className="flex wrap:flex-col flex-row wrap:overflow-y-auto overflow-y-scroll no-scrollbar">
+          <div className="flex m-2 wrap:flex-row flex-col">
+            <img src={require("./img.png")} alt="" width="60" className="m-auto" />
+            <div className="w-full flex flex-col">
+              <h1 className="text-center">Ondřej Hájek</h1>
+              <div className="px-2 rounded-md bg-logo-blue m-auto ">Follow</div>
+            </div>
+          </div>
 
           <div className="flex m-2 wrap:flex-row flex-col">
             <img src={require("./img.png")} alt="" width="60" className="m-auto" />
@@ -41,25 +41,41 @@ export default function ScrollPage() {
               <h1 className="text-center">Ondřej Hájek</h1>
               <div className="px-2 rounded-md bg-logo-blue m-auto ">Follow</div>
             </div>
+          </div>
 
+          <div className="flex m-2 wrap:flex-row flex-col">
+            <img src={require("./img.png")} alt="" width="60" className="m-auto" />
+            <div className="w-full flex flex-col">
+              <h1 className="text-center">Ondřej Hájek</h1>
+              <div className="px-2 rounded-md bg-logo-blue m-auto ">Follow</div>
+            </div>
+          </div>
 
+          <div className="flex m-2 wrap:flex-row flex-col">
+            <img src={require("./img.png")} alt="" width="60" className="m-auto" />
+            <div className="w-full flex flex-col">
+              <h1 className="text-center">Ondřej Hájek</h1>
+              <div className="px-2 rounded-md bg-logo-blue m-auto ">Follow</div>
+            </div>
+          </div>
 
+          <div className="flex m-2 wrap:flex-row flex-col">
+            <img src={require("./img.png")} alt="" width="60" className="m-auto" />
+            <div className="w-full flex flex-col">
+              <h1 className="text-center">Ondřej Hájek</h1>
+              <div className="px-2 rounded-md bg-logo-blue m-auto ">Follow</div>
+            </div>
+          </div>
         </div>
-
-      
       </div>
-      </div>
-
 
       <div className="wrap:w-main-box w-full flex flex-col order-3 wrap:order-2 m-2 wrap:m-0">
         <form className="flex flex-col w-full" onSubmit={submit}>
           <label className="text-xl font-semibold">Share how do you feel!</label>
-          <input type="text" className="bg-logo-blue text-white text-xl font-semibold outline-none p-1 mb-2" placeholder="Title..." onChange={change} value={contribution.title} name="title"/>
+          <input type="text" className="bg-logo-blue text-white text-xl font-semibold outline-none p-1 mb-2" placeholder="Title..." onChange={change} value={contribution.title} name="title" />
           <textarea className="w-full resize-none h-28 outline-none bg-logo-blue text-white p-1" placeholder="Content..." onChange={change} value={contribution.content} name="content"></textarea>
           <div className="my-2">
-            <button className="bg-logo-blue p-1 rounded-lg text-xl ml-auto">
-              Post!
-            </button>
+            <button className="bg-logo-blue p-1 rounded-lg text-xl ml-auto">Post!</button>
           </div>
         </form>
 
@@ -68,12 +84,17 @@ export default function ScrollPage() {
         </div>
       </div>
 
-
-<div className="wrap:w-side-box border-4 p-2 order-2 wrap:order-3 w-full h-min">
+      <div className="wrap:w-side-box border-4 p-2 order-2 wrap:order-3 w-1/2">
         <h1 className="font-semibold">Groups...</h1>
 
         <div className="flex wrap:flex-col flex-row wrap:overflow-y-auto overflow-y-scroll no-scrollbar">
-
+          <div className="flex m-2 wrap:flex-row flex-col">
+            <img src={require("./img.png")} alt="" width="60" className="m-auto" />
+            <div className="w-full flex flex-col">
+              <h1 className="text-center">Murdria</h1>
+              <div className="px-2 rounded-md bg-logo-blue m-auto">Follow</div>
+            </div>
+          </div>
 
           <div className="flex m-2 wrap:flex-row flex-col">
             <img src={require("./img.png")} alt="" width="60" className="m-auto" />
@@ -82,9 +103,32 @@ export default function ScrollPage() {
               <div className="px-2 rounded-md bg-logo-blue m-auto">Follow</div>
             </div>
           </div>
-      </div>
-</div>
 
+          <div className="flex m-2 wrap:flex-row flex-col">
+            <img src={require("./img.png")} alt="" width="60" className="m-auto" />
+            <div className="w-full flex flex-col">
+              <h1 className="text-center">Murdria</h1>
+              <div className="px-2 rounded-md bg-logo-blue m-auto">Follow</div>
+            </div>
+          </div>
+
+          <div className="flex m-2 wrap:flex-row flex-col">
+            <img src={require("./img.png")} alt="" width="60" className="m-auto" />
+            <div className="w-full flex flex-col">
+              <h1 className="text-center">Murdria</h1>
+              <div className="px-2 rounded-md bg-logo-blue m-auto">Follow</div>
+            </div>
+          </div>
+
+          <div className="flex m-2 wrap:flex-row flex-col">
+            <img src={require("./img.png")} alt="" width="60" className="m-auto" />
+            <div className="w-full flex flex-col">
+              <h1 className="text-center">Murdria</h1>
+              <div className="px-2 rounded-md bg-logo-blue m-auto">Follow</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
